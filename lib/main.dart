516b49a0d9afd0c4a1dd2fac9e7632f78ddb4785
GopalSaha21 <2220216@iub.edu.bd>
2025-06-26 22:33:33 +0600

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,12 +33,55 @@ class Home extends StatelessWidget {
         ),
         backgroundColor: Colors.pink,
       ),
-
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 100,),
+            SizedBox(height: 16,),
+            ElevatedButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      isScrollControlled: true,
+                      useSafeArea: true,
+                      enableDrag: false,
+                      builder: (ctx) {
+                        return Column(
+                          children: [
+                            Padding(padding: EdgeInsets.symmetric(vertical: 16),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Title",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              height: 20,
+                              thickness: 4,
+                            ),
+                            Text("Simple"),
+                            Row(
+                              children: [
+                                ElevatedButton(onPressed: (){}, child: Text("Cancel")),
+                                ElevatedButton(onPressed: (){}, child: Text("Save")),
+                              ],
+                            )
+                          ],
+                        );
+                      },
+                  );
+                },
+                child: Text("Show Dialog")),
           ],
         ),
       ),
